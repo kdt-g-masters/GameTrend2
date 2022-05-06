af77b508af714428abc858a50b3c3fd9b240b1f2
saeha <togk1996@gmail.com>
2022-05-06 16:42:09 +0900

    Create game pkg

---
+++ gametrend2/src/main/java/edu/kdt/gametrend/game/GameController.java
@@ -0,0 +1,32 @@
+package edu.kdt.gametrend.game;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GameController {
+	@Autowired
+	@Qualifier("gameservice")
+	GameService service;
+	
+	@RequestMapping("/list")
+	public ModelAndView gamelist(@RequestParam(required = false) Integer page) {
+		if(page == null) page = 1;
+		ModelAndView mv = new ModelAndView();
+		List<GameDTO> list = service.gamelist(page);
+		mv.addObject("paginglist",list);
+		mv.addObject("name", "페이징처리리스트");
+		mv.setViewName("index");
+		return mv;
+	}
+	
+	//@RequestMapping(value="/{platform}")
+	
+	
+}
