3eec1ddfe44ece8a13a7097cccccb29301033a26
joowon <joo1yeon@naver.com>
2022-05-06 15:40:34 +0900

    Spring boot 프로젝트 생성

---
+++ gametrend2/src/main/java/edu/kdt/gametrend/Gametrend2Application.java
@@ -0,0 +1,13 @@
+package edu.kdt.gametrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gametrend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gametrend2Application.class, args);
+	}
+
+}
---
+++ gametrend2/src/test/java/edu/kdt/gametrend/Gametrend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kdt.gametrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gametrend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
