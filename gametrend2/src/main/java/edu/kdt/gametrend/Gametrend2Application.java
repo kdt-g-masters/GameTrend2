fb49d1c8ac133a6f7c2c0726da51618e3493faff
DESKTOP-434QLLV\whdan <whdan33@naver.com>
2022-05-09 10:03:33 +0900

    수정 4

--- gametrend2/src/main/java/edu/kdt/gametrend/Gametrend2Application.java
+++ gametrend2/src/main/java/edu/kdt/gametrend/Gametrend2Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import edu.kdt.gametrend.calendar.CalendarController;
+import edu.kdt.gametrend.calendar.CalendarDAO;
 import edu.kdt.gametrend.carousel.CarouselController;
 import edu.kdt.gametrend.carousel.CarouselDAO;
 import edu.kdt.gametrend.game.GameController;
@@ -22,12 +24,14 @@ import edu.kdt.gametrend.review.ReviewDAO;
 @ComponentScan(basePackageClasses = CarouselController.class)
 @ComponentScan(basePackageClasses = MemberController.class)
 @ComponentScan(basePackageClasses = PromotionController.class)
+@ComponentScan(basePackageClasses = CalendarController.class)
 @ComponentScan(basePackageClasses = ReviewController.class)
 
 @MapperScan(basePackageClasses = GameDAO.class)
 @MapperScan(basePackageClasses = CarouselDAO.class)
 @MapperScan(basePackageClasses = MemberDAO.class)
 @MapperScan(basePackageClasses = PromotionDAO.class)
+@MapperScan(basePackageClasses = CalendarDAO.class)
 @MapperScan(basePackageClasses = ReviewDAO.class)
 public class Gametrend2Application {
 
