a4e0f1e0eda13617afcfbe8b435b997bfa1be037
DESKTOP-434QLLV\whdan <whdan33@naver.com>
2022-05-09 09:38:51 +0900

    수정 1

--- gametrend2/src/main/java/edu/kdt/gametrend/calendar/CalendarController.java
+++ gametrend2/src/main/java/edu/kdt/gametrend/calendar/CalendarController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CalendarController {
@@ -13,13 +14,18 @@ public class CalendarController {
 	@Qualifier("CalendarServiceImpl")
 	CalendarService service;
 	
-	
 	// 달력 페이지 호출  
 	@RequestMapping(value="/calendar", method=RequestMethod.GET) 
 		public String calendarAction1() {
 			return "calendar"; 
 	}
+	
+	@RequestMapping(value="/calendar", method=RequestMethod.POST)
+	public ModelAndView calendarAction2() { 
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("calendar");
+		return mv;
+	}
 
 }
-
 
