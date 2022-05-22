0b86d0a4bd467af7d58ef881fb6b6db57b379c7f
inacro <kpaks24@naver.com>
2022-05-23 06:30:35 +0900

    게임 상세 페이지 프론트 엔드 수정, 코드 리팩토링

--- gametrend2/src/main/java/edu/kdt/gametrend/review/ReviewController.java
+++ gametrend2/src/main/java/edu/kdt/gametrend/review/ReviewController.java
@@ -1,6 +1,11 @@
 package edu.kdt.gametrend.review;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -36,8 +42,32 @@ public class ReviewController {
 	@RequestMapping(value = "/insertReview", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
 	@ResponseBody
 	public String insertReview(ReviewDTO dto) {
+		//리뷰 유무 확인
 		int check = service.reviewCheck(dto);
 		if(check == 0) {
+			/*
+			// 파일 저장 경로
+			String resourceSrc = request.getServletContext().getRealPath("");
+			String savePath = resourceSrc.substring(0, resourceSrc.lastIndexOf("webapp")) + "resources\\static\\images\\review";
+			
+			if (!image.isEmpty()) {
+				// 파일 이름
+				String originName = image.getOriginalFilename();			
+				String ext = originName.substring(originName.indexOf("."));
+				UUID uuid = UUID.randomUUID();
+				String fileName = uuid + ext;
+				
+				// 파일 저장
+				File serverfile = new File(savePath, fileName);
+				image.transferTo(serverfile);
+				
+				// dto에 파일 정보 설정
+				dto.setImage(fileName);
+			}
+			else {
+				dto.setImage(null);
+			}
+			*/
 			int result = service.insertReview(dto);
 			if(result == 1) { 
 				return "{\"check\":\"리뷰 작성 완료\"}"; 
@@ -47,7 +77,7 @@ public class ReviewController {
 			}
 		}
 		else {
-			return "no";
+			return "{\"check\":\"리뷰 이미 존재함\"}";
 		}
 	}
 	
